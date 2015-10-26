4ccc9d3f83f4d95d40fda30a5b5643acfd7b5830
Andrew Siegle <asiegle@outlook.com>
2015-10-25 21:19:10 -0700

    changing for file structure

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.post('/setcolor', function(req, res, next) {
   var ip = req.body.ip;
   var color = req.body.color.slice(1);
   var options = { args: [color.slice(0, 2), color.slice(2, 4), color.slice(4, 6), ip] };
-  PythonShell.run('C:\\Users\\Andrew\\rita\\arduinoEthernet.py', options, function(err, results) {
+  PythonShell.run('C:\\Users\\Andrew\\rita\\python_scripts\\arduinoEthernet.py', options, function(err, results) {
     if(err) {
       return res.json(err);
     }
